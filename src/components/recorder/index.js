5ee87157cb5f2ac0b8dec6e4371ad41ab92005a2
Jack Bittiner <jackbittiner@hotmail.com>
2019-12-01 15:35:46 +0000

    :sparkles: turn Mic off on request

--- src/components/recorder/index.js
+++ src/components/recorder/index.js
@@ -3,17 +3,29 @@ import React, {useState} from 'react';
 const Recorder = () => {
     const [audioContext, setAudioContext] = useState(null)
 
-     const getMicrophone = async () => {
-        const audio = await navigator.mediaDevices.getUserMedia({
-          audio: true,
-          video: false
-        });
-        setAudioContext({ audio });
+    const getMicrophone = async () => {
+      const audio = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false
+      });
+      setAudioContext({ audio });
+    }
+
+    const stopMicrophone = () => {
+      setAudioContext(null);
+    }
+
+    const toggleMicrophone = () => {
+      if (audioContext) {
+        stopMicrophone();
+      } else {
+        getMicrophone();
       }
+    }
 
     return(
         <div>
-            <button onClick={() => getMicrophone()}>Allow Access To Audio</button>
+            <button onClick={() => toggleMicrophone()}>{audioContext ? "Turn Mic Off" : "Turn Mic On"}</button>
         </div>
     )
 }
