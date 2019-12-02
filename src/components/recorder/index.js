61d28cef24b588b989ba9735bdba2ee6e694ea6e
Jack Bittiner <jackbittiner@hotmail.com>
2019-12-02 13:49:23 +0000

    :sparkles: start and stop recording audio

--- src/components/recorder/index.js
+++ src/components/recorder/index.js
@@ -1,34 +1,53 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const Recorder = () => {
-    const [audioContext, setAudioContext] = useState(null)
+  const [audioContext, setAudioContext] = useState(null);
+  const [mediaRecorder, setMediaRecorder] = useState(null);
+  const [recordingData, setRecordingData] = useState([]);
 
-    const getMicrophone = async () => {
-      const audio = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: false
-      });
-      setAudioContext(audio);
+  const getMicrophone = async () => {
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    });
+    const recorder = new MediaRecorder(audio);
+    setMediaRecorder(recorder);
+    setAudioContext(audio);
+  };
+
+  const stopMicrophone = () => {
+    audioContext.getTracks().forEach(track => track.stop());
+    setAudioContext(null);
+  };
+
+  const toggleMicrophone = () => {
+    if (audioContext) {
+      stopMicrophone();
+    } else {
+      getMicrophone();
     }
+  };
 
-    const stopMicrophone = () => {
-      audioContext.getTracks().forEach(track => track.stop());
-      setAudioContext(null);
-    }
+  const startRecording = () => {
+    mediaRecorder.start();
+    mediaRecorder.ondataavailable = function(e) {
+      setRecordingData(recordingData.concat(e.data));
+    };
+  };
 
-    const toggleMicrophone = () => {
-      if (audioContext) {
-        stopMicrophone();
-      } else {
-        getMicrophone();
-      }
-    }
+  const stopRecording = () => {
+    mediaRecorder.stop();
+  };
 
-    return(
-        <div>
-            <button onClick={() => toggleMicrophone()}>{audioContext ? "Turn Mic Off" : "Turn Mic On"}</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <button onClick={() => toggleMicrophone()}>
+        {audioContext ? "Turn Mic Off" : "Turn Mic On"}
+      </button>
+      <button onClick={() => startRecording()}>START RECORD SOMETHING</button>
+      <button onClick={() => stopRecording()}>STOP RECORD SOMETHING</button>
+    </div>
+  );
+};
 
 export default Recorder;
