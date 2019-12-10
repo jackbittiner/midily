52c33ab1627b18416c17a2152345b8b78f26989e
Jack Bittiner <jackbittiner@hotmail.com>
2019-12-10 18:56:31 +0000

    :sparkles: string to bytes function

---
+++ src/midi-generator/utils/stringToBytes.js
@@ -0,0 +1,15 @@
+export function str2Bytes(str, finalBytes) {
+  if (finalBytes) {
+    while (str.length / 2 < finalBytes) {
+      str = "0" + str;
+    }
+  }
+
+  let bytes = [];
+  for (let i = str.length - 1; i >= 0; i = i - 2) {
+    let chars = i === 0 ? str[i] : str[i - 1] + str[i];
+    bytes.unshift(parseInt(chars, 16));
+  }
+
+  return bytes;
+}
