278588eed2e22e76951a617cc77490b39bbf998e
Jack Bittiner <jackbittiner@hotmail.com>
2019-12-10 16:21:22 +0000

    :white_check_mark: tests for event channel

--- src/midi-generator/midi-event.test.js
+++ src/midi-generator/midi-event.test.js
@@ -4,7 +4,7 @@ describe("MidiEvent", function() {
   describe("type", function() {
     it("should error when incorrect type is input", function() {
       const constructor = () => {
-        new MidiEvent({ type: 0x70 });
+        new MidiEvent({ type: 0x70, channel: 10 });
       };
       try {
         constructor();
@@ -13,8 +13,24 @@ describe("MidiEvent", function() {
       }
     });
     it("should initialise with the type", function() {
-      const event = new MidiEvent({ type: 0x80 });
+      const event = new MidiEvent({ type: 0x80, channel: 10 });
       expect(event.type).toBe(128);
+    });
+  });
+  describe("type", function() {
+    it("should error when incorrect channel is input", function() {
+      const constructor = () => {
+        new MidiEvent({ channel: 100, type: 0x80 });
+      };
+      try {
+        constructor();
+      } catch (error) {
+        expect(error.message).toBe("100 is out of bounds.");
+      }
+    });
+    it("should initialise with the type", function() {
+      const event = new MidiEvent({ channel: 10, type: 0x80 });
+      expect(event.channel).toBe(10);
     });
   });
 });
