2a73680d6d5b993384267ca68c253366d186178c
Jack Bittiner <jackbittiner@hotmail.com>
2019-12-10 18:17:50 +0000

    :white_check_mark: addNoteOff test

--- src/midi-generator/midi-track/midi-track.test.js
+++ src/midi-generator/midi-track/midi-track.test.js
@@ -13,4 +13,16 @@ describe("MidiTrack", function() {
       expect(track.events[0].param2).toStrictEqual(90);
     });
   });
+  describe("addNoteOff", function() {
+    it("should add note on event with specified params", function() {
+      const track = new MidiTrack();
+      track.addNoteOff(1, 69, 50);
+      expect(track.events[0].param2).toStrictEqual(50);
+    });
+    it("should default velocity to 90 if not given", function() {
+      const track = new MidiTrack();
+      track.addNoteOff(1, 69);
+      expect(track.events[0].param2).toStrictEqual(90);
+    });
+  });
 });
