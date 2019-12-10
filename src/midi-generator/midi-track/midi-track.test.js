2074839ce5d19a7f9a89e973f4825f7f234e7f30
Jack Bittiner <jackbittiner@hotmail.com>
2019-12-10 18:12:47 +0000

    :white_check_mark: default velocity test

--- src/midi-generator/midi-track/midi-track.test.js
+++ src/midi-generator/midi-track/midi-track.test.js
@@ -7,5 +7,10 @@ describe("MidiTrack", function() {
       track.addNoteOn(1, 69, 50);
       expect(track.events[0].param2).toStrictEqual(50);
     });
+    it("should default velocity to 90 if not given", function() {
+      const track = new MidiTrack();
+      track.addNoteOn(1, 69);
+      expect(track.events[0].param2).toStrictEqual(90);
+    });
   });
 });
