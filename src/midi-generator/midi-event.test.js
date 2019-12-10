579c5f85f4b62bbd90706a052c569f3c0f06b789
Jack Bittiner <jackbittiner@hotmail.com>
2019-12-10 16:46:56 +0000

    :white_check_mark: write test for event param

--- src/midi-generator/midi-event.test.js
+++ src/midi-generator/midi-event.test.js
@@ -4,7 +4,7 @@ describe("MidiEvent", function() {
   describe("type", function() {
     it("should error when incorrect type is input", function() {
       const constructor = () => {
-        new MidiEvent({ type: 0x70, channel: 10 });
+        new MidiEvent({ type: 0x70, channel: 10, param1: 50 });
       };
       try {
         constructor();
@@ -13,14 +13,14 @@ describe("MidiEvent", function() {
       }
     });
     it("should initialise with the type", function() {
-      const event = new MidiEvent({ type: 0x80, channel: 10 });
+      const event = new MidiEvent({ type: 0x80, channel: 10, param1: 50 });
       expect(event.type).toBe(128);
     });
   });
   describe("type", function() {
     it("should error when incorrect channel is input", function() {
       const constructor = () => {
-        new MidiEvent({ channel: 100, type: 0x80 });
+        new MidiEvent({ channel: 100, type: 0x80, param1: 50 });
       };
       try {
         constructor();
@@ -29,8 +29,20 @@ describe("MidiEvent", function() {
       }
     });
     it("should initialise with the channel", function() {
-      const event = new MidiEvent({ channel: 10, type: 0x80 });
+      const event = new MidiEvent({ channel: 10, type: 0x80, param1: 50 });
       expect(event.channel).toBe(10);
+    });
+  });
+  describe("Param 1 and 2", function() {
+    it("should initialise with the channel", function() {
+      const event = new MidiEvent({
+        channel: 10,
+        type: 0x80,
+        param1: 20,
+        param2: 40
+      });
+      expect(event.param1).toBe(20);
+      expect(event.param2).toBe(40);
     });
   });
 });
