007599b48562eb14188956102c0c863f84cb6417
Jack Bittiner <jackbittiner@hotmail.com>
2019-12-10 19:06:54 +0000

    :recycle: rename midi file constants

--- src/midi-generator/constants/midi-file-constants.js
+++ src/midi-generator/constants/midi-file-constants.js
@@ -1,4 +1,4 @@
-export const MIDI_FILE = {
+export const MIDI_FILE_CONSTANTS = {
   HDR_CHUNKID: "MThd", // file magic cookie:
   HDR_CHUNK_SIZE: "\x00\x00\x00\x06", // Header length for SMF
   HDR_TYPE0: "\x00\x00", // Midi Type 0 id
