13e82566fdf7aaf750a6a080c08539e31d580470
Jack Bittiner <jackbittiner@hotmail.com>
2019-12-11 10:05:45 +0000

    :sparkles: midi file to bytes

--- src/midi-generator/midi-file/midi-file.js
+++ src/midi-generator/midi-file/midi-file.js
@@ -1,4 +1,8 @@
-class MidiTrack {
+import { MIDI_FILE_CONSTANTS } from "../constants/midi-file-constants";
+import { stringToBytes } from "../utils/stringToBytes";
+import { codesToString } from "../utils/codes-to-string";
+
+class MidiFile {
   constructor() {
     this.tracks = [];
   }
@@ -6,6 +10,24 @@ class MidiTrack {
   addTrack(track) {
     this.tracks.push(track);
   }
+
+  toBytes() {
+    let trackCount = this.tracks.length.toString(16);
+
+    let bytes =
+      MIDI_FILE_CONSTANTS.HDR_CHUNKID +
+      MIDI_FILE_CONSTANTS.HDR_CHUNK_SIZE +
+      MIDI_FILE_CONSTANTS.HDR_TYPE0;
+
+    bytes += codesToString(stringToBytes(trackCount, 2));
+    bytes += MIDI_FILE_CONSTANTS.HDR_SPEED;
+
+    this.tracks.forEach(function(track) {
+      bytes += codesToString(track.toBytes());
+    });
+
+    return bytes;
+  }
 }
 
-export default MidiTrack;
+export default MidiFile;
